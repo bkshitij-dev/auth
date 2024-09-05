16baf26d00dbeb06f6ca9d763d3e2f4a8cedd82f
Kshitij Bajracharya <kshitij.bajracharya@infodev.com.np>
2024-09-05 15:50:31 +0545

    Add custom user details

--- src/main/java/com/example/demoauth/security/AppUserDetailsService.java
+++ src/main/java/com/example/demoauth/security/AppUserDetailsService.java
@@ -3,15 +3,10 @@ package com.example.demoauth.security;
 import com.example.demoauth.model.User;
 import com.example.demoauth.repository.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -22,16 +17,8 @@ public class AppUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
         User user = userRepository.findByUsernameOrEmail(usernameOrEmail)
-                .orElseThrow(() -> new UsernameNotFoundException("User not exists by Username or Email"));
-
-        Set<GrantedAuthority> authorities = user.getRoles().stream()
-                .map((role) -> new SimpleGrantedAuthority(role.getName().name()))
-                .collect(Collectors.toSet());
-
-        return new org.springframework.security.core.userdetails.User(
-                usernameOrEmail,
-                user.getPassword(),
-                authorities
-        );
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User with username/email " + usernameOrEmail + " not found"));
+        return AppUserDetails.build(user);
     }
 }
